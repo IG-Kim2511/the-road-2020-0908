4e4b66c7ecebc005f650023e12129b4cc0cfa631
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-09-09 15:48:04 -0400

    css: Popular tours / js 02 popular tours button

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
+// js 02 popular tours button
 
-// js 6 navbar icon click - navbar wrapper coming out
+const navBtn = document.querySelectorAll('.navigation-button');
+
+
+Array.from(navBtn).forEach((item)=>{
+    item.onclick =()=>{
+        item.parentElement.parentElement.classList.toggle('change');
+    }
+
+}
+
+);
+
+
+
+// js 06 navbar icon click - navbar wrapper coming out
 
 const openNavbar = document.querySelector('.open-navbar-icon');
 const closeNavbar = document.querySelector('.close-navbar-icon');
