9c849175adf1c88d3fffb4fbc4632d68c72d747a
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-09-10 17:41:57 -0400

    js 04. put color on 'close navbar icon'

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-
 // js 02 popular tours button
 
-const navBtn = document.querySelectorAll('.navigation-button');
+const navBtn = document.querySelectorAll(".navigation-button");
 
+Array.from(navBtn).forEach((item) => {
+  item.onclick = () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
 
-Array.from(navBtn).forEach((item)=>{
-    item.onclick =()=>{
-        item.parentElement.parentElement.classList.toggle('change');
-    }
+// js 04. put color on 'close navbar icon'
 
-}
+const colors = ["red", "blue", "green", "yellow", "orange"];
 
-);
+let i = 0;
 
+const navLink = document.querySelectorAll(".nav-link");
 
+Array.from(navLink).forEach((item) => {
+  item.style.cssText = `background-color:${colors[i++]}`;
+});
 
 // js 06 navbar icon click - navbar wrapper coming out
 
-const openNavbar = document.querySelector('.open-navbar-icon');
-const closeNavbar = document.querySelector('.close-navbar-icon');
+// const openNavbar = document.querySelector('.open-navbar-icon');
+// const closeNavbar = document.querySelector('.close-navbar-icon');
 
-const container = document.querySelector('.container');
+const navbarIcon = document.querySelector(".navbar-icon");
+const container = document.querySelector(".container");
 
-openNavbar.addEventListener('click',()=>{
-container.classList.add('change');
+navbarIcon.addEventListener("click", () => {
+  container.classList.add("change");
 });
 
-closeNavbar.addEventListener('click',()=>{
-container.classList.add('change');
-});
+// closeNavbar.addEventListener('click',()=>{
+// container.classList.add('change');
+// });
